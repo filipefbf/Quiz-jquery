0464599347279a5d671128fd147de246e8d74f34
filipefbf <filipe_fbf@hotmail.com>
2022-04-06 08:41:29 -0300

    Quiz

---
+++ js/game.js
@@ -0,0 +1,17 @@
+/*variaveis de controle de perguntas*/
+var qtdPerguntas = 0;
+var perguntasFeitas = [];
+
+//PERGUNTAS DO JOGO
+const pergunta = [
+    {
+        pergunta: "Qual dessas linguagens não é uma linguagem de programação",
+        resposta: ["PHP", "JavaScript", "C++", "HTML"],
+        correta: "resp3"
+    }
+    {
+        pergunta: "Qual dessas linguagens não é uma linguagem de programação",
+        resposta: ["PHP", "JavaScript", "C++", "HTML"],
+        correta: "resp3"
+    }
+];
